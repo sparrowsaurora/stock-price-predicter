5e432736f8317c31fbb190ecd8a0e522f397b2ba
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-03-20 21:55:56 +0800

    added a few more functions

--- data/stock.py
+++ data/stock.py
@@ -1,3 +1,6 @@
+import yfinance as yf
+# https://yfinance-python.org/reference/yfinance.stock.html
+
 class Stock:
     def __init__(self, ticker:str, shares_owned:int = 0, invested:float = 0):
         # To make the stock item, which will be used to make the whole project work.
@@ -27,6 +30,24 @@ class Stock:
         # Gets the Current Price of a stock via API request
         current_price:float = 10.40 #temporary value
         return current_price
+    
+    def rawdata(self) -> list[str, float, list[float, float, float, float, float], int]:
+        #returns a list of all data in a raw format
+        current_price = self.get_current_price()
+        d5, d4, d3, d2, d1 = self.five_day_history()
+        return [self.ticker, current_price, [d5, d4, d3, d2, d1] ,self.shares_owned]
+    
+    def five_day_history(self) -> list[float, float, float, float, float]:
+        day5, day4, day3, day2, day1 = 0
+        return [day5, day4, day3, day2, day1]
+    
+    def month_history(self) -> dict[int]:
+
+        history = {
+            
+        }
+        return history
+
 
 apl = Stock("apl", 10, 500)
 print(apl.__str__())
