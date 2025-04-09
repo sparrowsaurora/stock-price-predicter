35e8d53c594bf4a199a2cff877f92533d1bf3a40
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-04-09 22:17:12 +0800

    added sentiment analysis base

---
+++ analysis/sentiment_analysis.py
@@ -0,0 +1,29 @@
+class Sentiment:
+    def __init__(self):
+        self.sentiment:float = 0
+        # A value -10 to 10 showing public sentiment of a stock.
+        self.MIN, self.MAX = -10, 10
+        # min and max value for sentiment
+    
+    def __repr__(self):
+        return f"Sentiment is at {self.sentiment * 10}%"
+    
+    def change_sentiment(self, value:float):
+        self.sentiment += value
+        self.sentiment = 10 if self.sentiment >= 10 else self.sentiment
+        self.sentiment = -10 if self.sentiment <= -10 else self.sentiment
+        print(self.sentiment)
+            
+    def get_sentiment(self):
+        news = []
+    
+    def get_sentiment(self):
+        return self.sentiment
+    
+    
+
+# s = Sentiment()
+# s.change_sentiment(9)
+# s.change_sentiment(-20)
+
+# print(s)
