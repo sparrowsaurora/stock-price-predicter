ee084844ffc5cbfa6d1db0351ac9afbc9f682bf2
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-07-28 00:49:10 +0800

    revamped formatting class

--- data/formating.py
+++ data/formating.py
@@ -1,34 +1,46 @@
 from termcolor import colored
+from tabulate import tabulate
 
-class Formatting:
-    def __init__(self):
-        pass
-    
+class Formatting:   
     @staticmethod
     def single(data) -> str:
         # a static method; returns a single formatted piece of data 
-        return f"-----------\n| {data:^8} |\n-----------"
+        # Ensure text is a string
+        data = str(data)
+        top = f"+{'─' * len(data)}+"
+        middle = f"│{data:^8}│"
+        bottom = f"+{'─' * len(data)}+"
+        return f"{top}\n{middle}\n{bottom}"
+
     
     @staticmethod
     def basic_stats(self, ticker, current_price, shares_owned, change_percent) -> str:
         # a static method; returns all the basic information in a formatted way
         # Uses
-        #   Formatting.percent_styleing to color "change-percent param"
+        #   Formatting._percent_styleing to color "change-percent param"
         # Params:
         #   self..
         #   Ticker - name of the stock: Str
         #   Current_price - current stock's price: Float
         #   Shares_owned - no. of shares owned: Int
         #   change_percent - the percent of change in the stock. 24h: Float
-        change_percent = self.percent_styling(change_percent)
-        return f"----------------------------\n| {ticker.upper()} | {current_price:.2f} | {shares_owned:,} | {change_percent} |\n----------------------------"
+        headers = ["Ticker", "Price", "Shares", "Change %"]
+
+        change_percent = self._percent_styling(change_percent)
+        data = [
+            [ticker.upper(), f"{current_price:.2f}", f"{shares_owned:,}", change_percent]
+        ]
+        return tabulate(data, headers=headers, tablefmt="grid")
     
-    def percent_styling(change_percent) -> str:
+    def _percent_styling(change_percent) -> str:
         # returns a coloured and signed version of change_percent
+        if change_percent == 0:
+            return colored(f"/ {change_percent}", "dark_grey")
         return colored("+" + str(change_percent), "green") if change_percent >= 0 else colored("-" + str(change_percent), "red")
     
     @staticmethod
-    def print_history(dict: dict) -> None:
-        for date, price in dict.items():
-            print(f"Date: {date}, Price: {price}")
+    def print_history(data: dict) -> str:
+        table = [[date, f"{price:.2f}"] for date, price in data.items()]
+        headers = ["Date", "Price"]
+        print(tabulate(table, headers=headers, tablefmt="orgtbl"))
             
