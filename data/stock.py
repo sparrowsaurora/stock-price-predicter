77e4df0b64d6671cd12a1713ae738b659a38acfe
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-07-29 23:40:22 +0800

    started working on display. added 2 html pages and abunch of json bases

--- data/stock.py
+++ data/stock.py
@@ -1,6 +1,6 @@
 # import yfinance as yf
 from tools.tools import Tools
-from data.formating import Formatting
+from data.formatting import Formatting
 
 '''REFOMAT TO USE ALPACA.MARKETS TRADING API'''
 
@@ -29,13 +29,29 @@ class Stock:
     
     # ===========================================================================================================
 
+    def get_stock_by_ticker(ticker: str):
+        # -> Stock
+        
+        
+        return ...
+
+
+    def rawdata(self) -> list[str, float, int, float]:
+        #returns a list of all data in a raw format
+        current_price = self.get_current_price()
+        change_percent = self.get_change_percent()
+        return [self.ticker, current_price, self.shares_owned, change_percent]
+    # [ticker.upper(), f"{current_price:.2f}", f"{shares_owned:,}", change_percent]
+
+    # ===========================================================================================================
+
     def get_current_price(self) -> float:
         # Gets the Current Price of a stock via API request
         current_price: float = 10.4 # temporary value ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         return current_price
     
     @Tools.timer
-    def history(self, length) -> dict:
+    def get_history(self, length) -> dict:
         raise NotImplementedError("we havent gotten this data from the api")
     
         # prep data to call api
@@ -55,16 +71,33 @@ class Stock:
     def get_change_percent(self) -> float:
         raise NotImplementedError("we havent gotten this data from the api")
         return change_percent
+        
+    def get_signal() -> str:
+        raise NotImplementedError
     
-    def rawdata(self) -> list[str, float, int, float]:
-        #returns a list of all data in a raw format
-        current_price = self.get_current_price()
-        change_percent = self.get_change_percent()
-        return [self.ticker, current_price, self.shares_owned, change_percent]
-    # [ticker.upper(), f"{current_price:.2f}", f"{shares_owned:,}", change_percent]
+    ################ PROPERTIES ######################
 
+    @property
     def display_formatted(self):
         """Formats and prints the stock data."""
         cont = self.rawdata()
         return Formatting.stock_base(cont[0], cont[1])
+    
+    @property
+    def ticker(self) -> str:
+        return self.ticker.upper()
+    
+    @property
+    def shares_owned(self) -> str:
+        return self.shares_owned
+
+    @property
+    def current_price(self) -> str:
+        return self.get_current_price()
+
+    @property
+    def signal(self) -> str:
+        return self.get_signal()
+
+
         
--- display/api.py
+++ display/api.py
@@ -1,25 +1,51 @@
 # Run with: uvicorn display/api:app --reload
 
+from data.stock import Stock
 
 
 
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="display/pages/static"), name="static")
 
-# GET endpoint
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"message": "Hello, world!"}
+    # make a graph image and project summary root page?
+    with open("display/pages/index.html", "r", encoding="utf-8") as f:
+        return f.read()
 
+@app.get("/about", response_class=HTMLResponse)
+def read_root():
+    # make a graph image and project summary root page?
+    with open("display/pages/about.html", "r", encoding="utf-8") as f:
+        return f.read()
 
+# GET endpoints
 @app.get("/{ticker}/")
 def get_stock(ticker: str):
-    return {"ticker": ticker.upper(), "message": f"Fetching data for {ticker.upper()}"}
+    stock = Stock.get_stock_by_ticker(ticker)
+    response = {"":""}
+    return response
+
+@app.get("/{ticker}/raw/")
+def get_stock(ticker: str):
+    # stock's data in raw format
+    stock = Stock.get_stock_by_ticker(ticker)
+    response = {
+        "ticker": stock.ticker,
+        "shares": stock.shares_owned,
+        "current_price": stock.current_price,
+        "signal": stock.signal
+        }
+    return response
 
 @app.get("/{ticker}/pretty/")
 def get_stock(ticker: str):
-    
-    return 
+    stock = Stock.get_stock_by_ticker(ticker)
+    return stock.display_formatted
 
 
 
